e3f81b29788c589a3f71b9536f5fb18fa9362a16
Henrique Amaral <hrsousa.dam@gmail.com>
2023-11-25 02:12:17 +0100

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,38 @@
+const localStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+//Model de usuario
+require('../models/Usuario')
+const Usuario = mongoose.model('usuarios')
+
+module.exports = function(passport){
+  passport.use(new localStrategy({usernameField: 'email', passwordField:'senha'},(email, senha, done)=>{
+    Usuario.findOne({email:email}).then((usuario)=>{
+      if(!usuario){
+        return done(null, false,{message: 'Esta conta não existe!'})
+      }
+
+      bcrypt.compare(senha, usuario.senha, (erro, batem)=>{
+        if(batem){
+          return done(null, usuario)
+        }else{
+          return done(null, false,{message:'Senha incorreta.'})
+        }
+      })
+    })
+  }))
+
+  passport.serializeUser((usuario, done)=>{
+    done(null, usuario.id)
+  })
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await Usuario.findById(id).exec();
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  })
+  }
---
+++ config/db.js
@@ -0,0 +1,6 @@
+if(process.env.Node_ENV=='production'){
+    module.exports = {mongoURI:"mongodb+srv://hrsousadam:<password>@cluster0.id10kwn.mongodb.net/"}
+  }else{
+    module.exports = {mongoURI:'mongodb://localhost/blogapp'}
+  }
+  
