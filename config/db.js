9b917dc4fe0342a0d0278f082c3afff03c0f473b
hrsousadam <hrsousa.dam@gmail.com>
2023-12-11 00:45:44 +0100

    mongodb 1.5

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
-if(process.env.Node_ENV=='production'){
-    module.exports = {mongoURI:"mongodb+srv://hrsousadam:<password>@cluster0.id10kwn.mongodb.net/"}
-  }else{
-    module.exports = {mongoURI:'mongodb://localhost:27017/blogapp'}
-  }
-  
+if (process.env.NODE_ENV === 'production') {
+  module.exports = { mongoURI: "mongodb+srv://hrsousadam:<password>@cluster0.id10kwn.mongodb.net/" };
+} else {
+  module.exports = { mongoURI: 'mongodb://localhost:27017/blogapp' };
+}
