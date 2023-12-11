317acf84716c8403a300ddec50e90b6e76704fba
hrsousadam <hrsousa.dam@gmail.com>
2023-12-11 01:38:12 +0100

    mongodb 1.7

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: "mongodb+srv://hrsousadam:<password>@cluster0.id10kwn.mongodb.net/" };
+  module.exports = { mongoURI: "mongodb+srv://hrsousadam:<Mongo1234>@cluster0.id10kwn.mongodb.net/" };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost:27017/blogapp' };
 }
